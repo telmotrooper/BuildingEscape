05e3336e59bad885c0f6775b9c09d23485bbb86d
Telmo <telmo.trooper@gmail.com>
2017-01-30 01:44:27 -0200

    Started "PositionReporter" and removed extra content

---
+++ Source/BuildingEscape/PositionReporter.h
@@ -0,0 +1,26 @@
+// Copyright 2017 Telmo Valverde
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "PositionReporter.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UPositionReporter : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UPositionReporter();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+		
+	
+};
